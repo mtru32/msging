ee20b27081af329418c1df007ae6bd0a92b59a73
mtru32 <mark.truttmann32@gmail.com>
2020-08-04 18:56:49 -0500

    Phone numbers
    Abstracted phone numbers

--- texting.py
+++ texting.py
@@ -4,6 +4,8 @@ from twilio.rest import Client
 with open('C:/Users/Mark/desktop/twilAuth.txt', 'r') as f:
     account_sid = f.readline()
     auth_token = f.readline()
+    twil = f.readline()
+    me = f.readline()
 
 # Your Account Sid and Auth Token from twilio.com/console
 client = Client(account_sid, auth_token)
@@ -11,8 +13,8 @@ client = Client(account_sid, auth_token)
 message = client.messages \
                 .create(
                      body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+18139061773',
-                     to='+16082129272'
+                     from_= twil,
+                     to= me
                  )
 
 print(message.sid)
